fc3fb8f114b96721de4e82c3e5b95263dd4e339c
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-04-24 14:15:41 +0300

    folder structure Setup

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="text-red-800">hello</div>;
-}
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//pages
+import HomePage from "./pages/home-page";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
