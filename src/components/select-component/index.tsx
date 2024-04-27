ac4f6d98a2221a6301f6e0a42aa0fc26ab9b3427
ikbal arslan <ikbalarslangamedesigner@gmail.com>
2024-04-27 15:22:41 +0300

    input design

--- src/components/select-component/index.tsx
+++ src/components/select-component/index.tsx
@@ -1,6 +1,7 @@
 import { UseQueryResult } from "@tanstack/react-query";
 import { IQueryData } from "../../types";
 import { useSearchParams } from "react-router-dom";
+import { FaCaretDown } from "react-icons/fa";
 
 const SelectComponent: React.FC<{ queryData: UseQueryResult<IQueryData> }> = ({
   queryData,
@@ -12,15 +13,19 @@ const SelectComponent: React.FC<{ queryData: UseQueryResult<IQueryData> }> = ({
   };
 
   return (
-    <div>
-      {queryData.data?.info?.count}
-      <input
-        type="text"
-        className="border-2"
-        value={searchParams.get("name") || ""}
-        onChange={handleInputChange}
-        placeholder="Enter query..."
-      />
+    <div className="flex flex-col gap-1 text-gray-500">
+      {queryData.data?.info?.count || 0}
+
+      <div className="flex border p-1 rounded-xl w-[20rem] border-gray-700   ">
+        <input
+          type="text"
+          className=" focus:ring-0 focus:outline-none w-full rounded-lg h-10 min-w-14"
+          value={searchParams.get("name") || ""}
+          onChange={handleInputChange}
+          placeholder="Enter query..."
+        />
+        <FaCaretDown className="self-center mr-1" size={22} />
+      </div>
     </div>
   );
 };
